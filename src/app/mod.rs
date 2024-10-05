b9efbc1f994650eaa9bdd4d56455ad299cca60a6
Artiom Khandamirov <t9max@yandex.ru>
2024-10-05 22:25:31 +0300

    Update dependencies, etc

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,8 +1,10 @@
 #[allow(non_snake_case)]
+#[allow(unreachable_patterns)]
 pub mod api;
 pub mod db;
 pub mod storage;
 pub mod templates;
 #[allow(non_snake_case)]
 #[allow(clippy::redundant_locals)]
+#[allow(unreachable_patterns)]
 pub mod views;
